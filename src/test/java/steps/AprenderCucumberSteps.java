b1cf36e92000825d376e9333be0a5ea93b77a183
lfernandes25 <leanstorm@hotmail.com>
2023-01-03 10:39:04 -0300

    CUJ004 - Cenarios do cucumber passando parametros

--- src/test/java/steps/AprenderCucumberSteps.java
+++ src/test/java/steps/AprenderCucumberSteps.java
@@ -3,8 +3,11 @@ package steps;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
+import org.junit.Assert;
 
 public class AprenderCucumberSteps {
+
+    private int contador = 0;
 
     @Dado("que criei o arquivo corretamente")
     public void queCrieiOArquivoCorretamente() {
@@ -18,5 +21,18 @@ public class AprenderCucumberSteps {
 
     @Entao("a especificacao deve finalizar com sucesso")
     public void aEspecificacaoDeveFinalizarComSucesso() {System.out.println("Passei pelo Entao");
+    }
+
+    @Dado("que o valor do contador e {int}")
+    public void queOValorDoContadorE(Integer valor) {
+        contador = valor;
+    }
+    @Quando("eu incrementar em {int}")
+    public void euIncrementarEm(Integer valor) {
+        contador += valor;
+    }
+    @Entao("o valor do contador sera {int}")
+    public void oValorDoContadorSera(int valor) {
+        Assert.assertEquals(contador,valor);
     }
 }
--- src/test/resources/aprender_cucumber.feature
+++ src/test/resources/aprender_cucumber.feature
@@ -6,3 +6,8 @@
       Dado que criei o arquivo corretamente
       Quando executa-lo
       Entao a especificacao deve finalizar com sucesso
+
+    Cenario: Deve incrementar contador
+      Dado que o valor do contador e 15
+      Quando eu incrementar em 3
+      Entao o valor do contador sera 18
